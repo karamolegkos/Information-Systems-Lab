ae471278c8018a257c7ac3d35cd4c1074f2f507d
karamolegkos <p.karamolegos@yahoo.gr>
2024-04-11 13:22:02 +0300

    Added examples for the while loop.

---
+++ lab1/while_loop/simple_while.py
@@ -0,0 +1,9 @@
+# Simple while loop example
+count = 1  # Initialize a counter variable
+
+# Keep looping until the count reaches 5
+while count <= 5:
+    print(count)  # Print the current value of count
+    count += 1    # Increment the count by 1 in each iteration
+
+print("Loop completed!")  # This line will execute after the loop exits
---
+++ lab1/while_loop/while_and_input.py
@@ -0,0 +1,8 @@
+# Simple while loop example
+user_input = ''  # Initialize the variable to hold user input
+
+# Keep asking for input until the user enters 'quit'
+while user_input != 'quit':
+    user_input = input("Enter 'quit' to exit: ")
+
+print("You've exited the loop!")  # This line will execute after the loop exits
