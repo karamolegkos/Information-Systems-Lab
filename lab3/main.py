c9612bce47a74267b6f03412e86a9fd3cd2d885e
antonisk6 <antonispardos@gmail.com>
2024-04-24 09:47:37 +0300

    Server runs on 0.0.0.0

--- lab3/main.py
+++ lab3/main.py
@@ -7,4 +7,4 @@ SECRET_KEY = os.environ.get('SECRET_KEY') or 'this is a secret'
 server.config['SECRET_KEY'] = SECRET_KEY
 
 if __name__ == "__main__":
-    server.run(port=5000, debug=True)
+    server.run(host="0.0.0.0", port=5000, debug=True)
