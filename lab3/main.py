be03d32e243d59d1543880c49724b484d97217a1
Panagiotis Karamolegkos <p.karamolegos@yahoo.gr>
2024-04-25 18:24:28 +0300

    Commented 0.0.0.0

--- lab3/main.py
+++ lab3/main.py
@@ -7,4 +7,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY') or 'this is a secret'
 server.config['SECRET_KEY'] = SECRET_KEY
 
 if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=5000, debug=True)
+    # If we use 0.0.0.0 as host, we are exposing our server to the outside world.
+    # server.run(host="0.0.0.0", port=5000, debug=True)
+    server.run(port=5000, debug=True)
