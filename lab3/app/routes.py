489af11c8bb91f26336f29ff5c642cfaf759fca6
Panagiotis Karamolegkos <p.karamolegos@yahoo.gr>
2024-04-25 18:28:05 +0300

    Made some more explanation using comments

--- lab3/app/routes.py
+++ lab3/app/routes.py
@@ -8,27 +8,45 @@ from app.middleware import token_required
 
 db = Repository()
 
-# Routes    
+# Routes
+# Get all the books
+@server.route('/books/all', methods=['GET'])
+def get_all_books():
+    books = db.get_books()
+    return books, 200
+
+
+# Get a book by id
 @server.route('/books', methods=['GET'])
-def books():
+def get_book():
+    # The endpoint should be something like the following: http://localhost:5000/books?id=95c57eed-6c06-4ed5-a44b-00073063628b
     # id = request.args.get('id')
+    # Check repository.py for the methods of the db object
+    # Check the model/books.py for the methods of a book object
     # Υλοποιήστε τον κώδικα σας
     pass
 
 
+# Add a book to the library
 @server.route('/books', methods=['POST'])
-def book():
+def add_book():
     # data = request.get_json()
     # Υλοποιήστε τον κώδικα σας
     pass
 
 
+# Delete a book from the library
 @server.route('/books/<id>', methods=['DELETE'])
 def delete_book(id):
+    # The endpoint should be something like the following: http://localhost:5000/books?id=95c57eed-6c06-4ed5-a44b-00073063628b
+    # The id parameter is being gathered by the function delete_book(id)
+    # Υλοποιήστε τον κώδικα σας
     pass
 
 
+# Update a book in the library. This function is using token authentication
 @server.route('/books/<id>', methods=['PUT'])
+@token_required
 def update_book(id):
     # data = request.get_json()
     # Υλοποιήστε τον κώδικα σας
